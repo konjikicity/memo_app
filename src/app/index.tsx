d10604fea91041ea2969ac5959c84a344214ad91
syu.onoda <s-onoda@creative-innovation.co.jp>
2024-12-14 16:21:05 +0900

    add: memo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,40 +3,40 @@ import { View, Text, StyleSheet } from "react-native";
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View></View>
-      <View>
-        <Text>Meme App</Text>
-        <Text>ログアウト</Text>
-      </View>
-
-      <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2024年10月1日</Text>
-        </View>
-        <View>
-          <Text>×</Text>
-        </View>
-      </View>
-      <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2024年10月1日</Text>
-        </View>
-        <View>
-          <Text>×</Text>
-        </View>
-      </View>
-      <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2024年10月1日</Text>
-        </View>
-        <View>
-          <Text>×</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Meme App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2024年10月1日</Text>
+        </View>
+        <View>
+          <Text>×</Text>
+        </View>
+      </View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2024年10月1日</Text>
+        </View>
+        <View>
+          <Text>×</Text>
+        </View>
+      </View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2024年10月1日</Text>
+        </View>
+        <View>
+          <Text>×</Text>
+        </View>
+      </View>
       <View>
         <Text>+</Text>
       </View>
@@ -47,8 +47,47 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#ffffff",
+  },
+  header: {
+    backgroundColor: "#467FD3",
+    height: 104,
+    justifyContent: "flex-end",
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  headerInner: {
     alignItems: "center",
+  },
+  headerRight: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color: "rgba(255, 255, 255, 0.7)",
+  },
+  memoListItem: {
+    backgroundColor: "#ffffff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "rgba(0,0,0,0.15)",
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#848484",
   },
 });
 export default Index;
